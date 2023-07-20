87ca975cbb4684a9549d97f1b28e66e1bb537488
Anna S <anna@mail.com>
2023-07-20 17:22:29 +0300

    20.07.23 первоначальная настройка и создание api

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,11 +1,20 @@
 import { Module } from '@nestjs/common';
-
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AuthController } from './controllers/auth.controller';
+import { ConfigModule } from '@nestjs/config';
+import { getRMQConfig } from './configs/rmq.config';
+import { RMQModule } from 'nestjs-rmq';
+import { JwtModule } from '@nestjs/jwt';
+import { getJWTConfig } from './configs/jwt.config';
+import { PassportModule } from '@nestjs/passport';
+import { UserController } from './controllers/user.controler';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({ envFilePath: 'envs/.api.env', isGlobal: true }),
+    RMQModule.forRootAsync(getRMQConfig()),
+    JwtModule.registerAsync(getJWTConfig()),
+    PassportModule,
+  ],
+  controllers: [AuthController, UserController],
 })
 export class AppModule {}
